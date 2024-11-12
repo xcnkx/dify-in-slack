5beff15ae0441a043e3d6dda726b87742b988e8e
c-kamia <c.kamia@cartahd.com>
2024-11-12 23:45:40 +0900

    first commit

---
+++ app/bot_listeners.py
@@ -0,0 +1,38 @@
+from slack_bolt import Ack, BoltContext
+from slack_sdk import WebClient
+import logging
+
+from app.slack_ops import find_parent_message, is_this_app_mentioned
+
+
+def just_ack(ack: Ack):
+    ack()
+
+
+def respond_to_app_mention(
+    context: BoltContext,
+    payload: dict,
+    client: WebClient,
+    logger: logging.Logger,
+):
+    thread_ts = payload.get("thread_ts")
+    if thread_ts is not None:
+        parent_message = find_parent_message(client, context.channel_id, thread_ts)
+        if parent_message is not None and is_this_app_mentioned(
+            context, parent_message
+        ):
+            # The message event handler will reply to this
+            return
+
+    try:
+        client.chat_postMessage(
+            channel=context.channel_id,
+            text="Hello there!",
+        )
+    except Exception as e:
+        logger.error(f"Failed to respond to app mention: {e}")
+
+        client.chat_postMessage(
+            channel=context.channel_id,
+            text="Sorry, I failed to respond to your message. Please try again later.",
+        )
---
+++ app/index.py
@@ -0,0 +1,25 @@
+import os
+
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+
+slack_app_token = slack_bot_token = os.environ["SLACK_APP_TOKEN"]
+slack_signing_token = os.environ["SLACK_SIGNING_SECRET"]
+
+app = App(
+    process_before_response=True,
+    token=slack_bot_token,
+    signing_secret=slack_signing_token,
+)
+
+
+def lambda_handler(event, context):
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
+
+
+if __name__ == "__main__":
+    from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+    SocketModeHandler(app, slack_app_token).start()
